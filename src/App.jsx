2711cc769a46c6cb7f3be540589b4842971aab6a
Eren <erencort1@gmail.com>
2023-06-18 22:41:22 +0300

    styling and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import BmiCalculate from "./components/BmiCalculate";
+import SideBarNav from "./components/SideBarNav";
+import { Routes, Route } from "react-router-dom";
+import WhatIsBmi from "./components/WhatIsBmi";
+import BmiResults from "./components/BmiResults";
+
 function App() {
-  return <div className="text-3xl font-bold underline">bmi</div>;
+  return (
+    <div
+      style={{ backgroundColor: "#242424" }}
+      className="h-screen text-white font-serif"
+    >
+      <div className="grid grid-cols-4 gap-4">
+        {/*Sidebar*/}
+        <div className="border-r-4 border-green-600 h-screen">
+          <SideBarNav />
+        </div>
+
+        <div className="col-span-3">
+          <Routes>
+            <Route path="/" element={<BmiCalculate />} />
+            <Route path="/whatisbmi" element={<WhatIsBmi />} />
+            <Route path="/results" element={<BmiResults />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SideBarNav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function SideBarNav() {
+  return (
+    <div>
+      <div className="text-3xl text-center py-5 border-b-4 border-green-600">
+        BMI CALCULATOR
+      </div>
+      <div>
+        <ul>
+          <Link to="/">
+            <li className="border-2 border-black bg-gray-800 hover:bg-gray-500 px-3 py-3 flex justify-between">
+              <span>Calculate BMI</span> <span>{">"}</span>
+            </li>
+            <Link to="/whatisbmi">
+              <li className="border-2 border-black bg-gray-800 hover:bg-gray-500 px-3 py-3 flex justify-between">
+                <span>What is BMI</span> <span>{">"}</span>
+              </li>
+            </Link>
+          </Link>
+          <Link to="results">
+            <li className="border-2 border-black bg-gray-800 hover:bg-gray-500 px-3 py-3 flex justify-between">
+              <span>Records</span> <span>{">"}</span>
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default SideBarNav;
